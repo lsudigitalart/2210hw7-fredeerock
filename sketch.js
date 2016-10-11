863cd0e74de74ceffae8316debb9cbba6d1a9ad2
Derick Ostrenko <dostrenko@gmail.com>
2016-10-11 10:53:11 -0500

    actual timing

--- sketch.js
+++ sketch.js
@@ -1,9 +1,36 @@
+var mySound;
+var once = 1;
+var actualTime;
+var loadTime;
+
 function setup() {
-  createCanvas(320, 240);
+  frameRate(60);
+  createCanvas(640, 480);
   background(0);
-  ellipse(width/2, height/2, 100);
+  mySound = loadSound("bolero.mp3", playMusic);
 }
 
 function draw() {
+  background(0);
+  actualTime = millis() - loadTime;
+  println(actualTime);
 
+  if(actualTime > 1000 && actualTime < 2000){
+    ellipse(100, 100, 100);
+  }
+}
+
+function playMusic(){
+  loadTime = millis();
+  mySound.play();
+}
+
+function mouseReleased(){
+  if(once == 1){
+    mySound.pause();
+    once = 0;
+  } else {
+    mySound.play();
+    once = 1;
+  }
 }
